5576f2d5c8351e3b97e65c89ab6d3f9795e52a31
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-07-09 11:53:12 +0900

    Youtubeダウンロードするやつを統合したい...

--- src/youtube.js
+++ src/youtube.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require("ytdl-core");
 const app = express();
+
 app.use(cors());
 app.listen(3000, () => {
     console.log('Server Works !!! At port 3000');
